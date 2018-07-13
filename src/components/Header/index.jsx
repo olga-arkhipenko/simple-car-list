2baf8ddfd632a79fef9b484f8bb9a226853a380a
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-13 19:25:35 +0300

    Added components folder.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <p className="logo">
+                Car list 
+            </p>
+        </header>
+    )
+};
+
+export default Header;
