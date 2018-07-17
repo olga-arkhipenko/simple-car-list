700a746848ef796e8cdf9238c455e5fbbcd421cb
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-17 10:38:19 +0300

    Refactored table heading.

--- src/components/TableHeading/index.jsx
+++ src/components/TableHeading/index.jsx
@@ -3,15 +3,17 @@ import './style.css';
 
 const TableHeading = () => {
     return (
-        <tr>
-            <th> Marque </th>
-            <th> Model </th>
-            <th> Production year </th>
-            <th> Engine capacity </th>
-            <th> Engine power </th>
-            <th> Price </th>
-            <th> Owner </th>
-        </tr>
+        <thead>
+            <tr>
+                <th> Marque </th>
+                <th> Model </th>
+                <th> Production year </th>
+                <th> Engine capacity </th>
+                <th> Engine power </th>
+                <th> Price </th>
+                <th> Owner </th>
+            </tr>
+        </thead>
     )
 };
 
