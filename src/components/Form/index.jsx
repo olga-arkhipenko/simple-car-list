d62077bfecaf9c8da8053e055869b05f39e2c6c0
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-18 02:11:33 +0300

    Added form styling.

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './style.css';
+import FormField from '../FormField';
 
 const Form = () => {
     return (
-        <form>
-            <input type="text" name=""/>
+        <form className="form">
+            <FormField name="Brand" type="text"/>
+            <FormField name="Model" type="text" />
+            <FormField name="Production year" type="number" />
+            <FormField name="Engine capacity" type="number" />
+            <FormField name="Engine power" type="number" />
+            <FormField name="Price" type="number" />
+            <FormField name="Owner" type="text" />
         </form>
     )
 };
