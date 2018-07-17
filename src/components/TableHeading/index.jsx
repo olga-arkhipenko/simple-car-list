64006d62327aec2ad843f8caf16741b884c5e2ae
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-17 10:30:38 +0300

    Added separate component for table heading.

---
+++ src/components/TableHeading/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const TableHeading = () => {
+    return (
+        <tr>
+            <th> Marque </th>
+            <th> Model </th>
+            <th> Production year </th>
+            <th> Engine capacity </th>
+            <th> Engine power </th>
+            <th> Price </th>
+            <th> Owner </th>
+        </tr>
+    )
+};
+
+export default TableHeading;
