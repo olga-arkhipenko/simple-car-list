1f133bc5114cead17e05a3f3ebea8c1c6979bb1f
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-17 16:57:47 +0300

    Changed logo name.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="header">
             <p className="logo">
-                Car list 
+                Cars list 
             </p>
         </header>
     )
