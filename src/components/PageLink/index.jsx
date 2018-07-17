26aba5ea4e215ca9efd031e582079fb301dc015a
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-17 20:12:31 +0300

    Created link component.

---
+++ src/components/PageLink/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+const PageLink = ({ path, name, type }) => {
+    return (
+        <Link to={ path } className={ `link ${ type }` }>
+            { name }
+        </Link>
+    )
+};
+
+export default PageLink;
