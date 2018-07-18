8e188df365baaaa6365c49af7cddac2f90711edd
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-18 03:11:14 +0300

    Added table cells styling.

--- src/components/CarsRow/index.jsx
+++ src/components/CarsRow/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './style.css';
 
-const TableRow = ({ car }) => {
+const CarsRow = ({ car }) => {
     return (
         <tr>
-            <th> { car.brand } </th>
-            <th> { car.model } </th>
-            <th> { car.productionYear } </th>
-            <th> { car.engineCapacity } </th>
-            <th> { car.enginePower } </th>
-            <th> { car.price } </th>
-            <th> { car.owner } </th>
+            <td className="table-cell table-cell--first" > { car.brand } </td>
+            <td className="table-cell" > { car.model } </td>
+            <td className="table-cell" > { car.productionYear } </td>
+            <td className="table-cell" > { car.engineCapacity } </td>
+            <td className="table-cell" > { car.enginePower } </td>
+            <td className="table-cell" > { car.price } </td>
+            <td className="table-cell table-cell--last" > { car.owner } </td>
         </tr>
     )
 };
 
-export default TableRow;
+export default CarsRow;
