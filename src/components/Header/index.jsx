dfa6f155d50a4de61c660281748348953699aa0e
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-18 12:13:47 +0300

    Added root link to logo.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../constants/routes';
 import './style.css';
 
 const Header = () => {
     return (
         <header className="header">
-            <p className="logo">
+            <Link to={ routes.HOME }>
+                <p className="logo">
                 Cars list 
-            </p>
+                </p>
+            </Link>
         </header>
     )
 };
