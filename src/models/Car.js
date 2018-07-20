56ede1c787e405d5167d29c18c51c205283997ed
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-20 12:03:44 +0300

    Added car model.

---
+++ src/models/Car.js
@@ -0,0 +1,12 @@
+class Car {
+    id = null;
+    brand = null;
+    model = null;
+    productionYear = null;
+    engineCapacity = null;
+    enginePower = null;
+    price = null;
+    owner = null;
+}
+
+export default Car;
