767acdc138590d058fc36937a4d460e2f5bde300
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-20 12:03:26 +0300

    Added car mapping.

---
+++ src/mappers/carMapper.js
@@ -0,0 +1,18 @@
+import Car from '../models/Car';
+import uuidv1 from 'uuid/v1';
+
+const mapToCar = (data) => {
+    const car = new Car();
+    car.id = uuidv1();
+    car.brand = data.brand;
+    car.model = data.model;
+    car.productionYear = data.productionYear;
+    car.engineCapacity = data.engineCapacity;
+    car.enginePower = data.enginePower;
+    car.price = data.price;
+    car.owner = data.owner;
+
+    return car;
+};
+
+export default { mapToCar };
