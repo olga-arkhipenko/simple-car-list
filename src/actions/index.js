091f59c97f7b0d4cd153c5e7f3af7b41bc5120cd
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-20 12:04:38 +0300

    Refactored car adding.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 import actionTypes from './actionTypes';
+import carMapper from '../mappers/carMapper';
 
-const addCar = (car) => ({
-    type: actionTypes.ADD_CAR,
-    car
-});
+const addCar = (data) => {
+    const car = carMapper.mapToCar(data);
+    return {
+        type: actionTypes.ADD_CAR,
+        car
+    }
+};
 
 export default addCar;
