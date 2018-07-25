1cb1cd3eb28e00e7e302e3e7f799c2b901508421
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-25 18:19:51 +0300

    Added redux dev tools.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const configureStore = () => {
     return createStore(
-        rootReducer
-    );
+        rootReducer,
+        composeWithDevTools(applyMiddleware())
+    )
 };
 
 export default configureStore;
