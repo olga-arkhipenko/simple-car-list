d0ce4abc9dbcf8dffb3d8cd06cc62cab4b183a5f
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-26 19:34:38 +0300

    Imported weather widget reducer.

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import carsReducer from './carsReducer';
+import weatherWidgetReducer from '../../features/weatherWidget/reducers';
 
 const rootReducer =  combineReducers({
-    cars: carsReducer
+    cars: carsReducer,
+    weatherData: weatherWidgetReducer
 });
 
 export default rootReducer;
