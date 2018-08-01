7404445a893a3151e153e6616df47ca96c0c9b60
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-01 23:29:31 +0300

    Destructured car object.

--- src/app/components/CarsRow/index.jsx
+++ src/app/components/CarsRow/index.jsx
@@ -3,31 +3,44 @@ import PageLink from '../PageLink';
 import routes from '../../constants/routes';
 import './index.css';
 
-const CarsRow = ({ car, onClickDelete }) => (
-    <tr>
-        <td className="table-cell table-cell--first"> { car.brand } </td>
-        <td className="table-cell"> { car.model } </td>
-        <td className="table-cell"> { car.productionYear } </td>
-        <td className="table-cell"> { car.engineCapacity } </td>
-        <td className="table-cell"> { car.enginePower } </td>
-        <td className="table-cell"> { car.price } </td>
-        <td className="table-cell table-cell--last"> { car.owner } </td>
-        <td className="edit-table-cell">
-            <span
-                className="edit-table-cell__icon edit-table-cell__icon-trash"
-                onClick={() => onClickDelete(car.id)}
-            >
+const CarsRow = ({ car, onClickDelete }) => {
+    const {
+        id,
+        brand,
+        model,
+        productionYear,
+        engineCapacity,
+        enginePower,
+        price,
+        owner
+    } = car;
+
+    return (
+        <tr>
+            <td className="table-cell table-cell--first"> {brand} </td>
+            <td className="table-cell"> {model} </td>
+            <td className="table-cell"> {productionYear} </td>
+            <td className="table-cell"> {engineCapacity} </td>
+            <td className="table-cell"> {enginePower} </td>
+            <td className="table-cell"> {price} </td>
+            <td className="table-cell table-cell--last"> {owner} </td>
+            <td className="edit-table-cell">
+                <span
+                    className="edit-table-cell__icon edit-table-cell__icon-trash"
+                    onClick={() => onClickDelete(id)}
+                >
                     &#9851;
-            </span>
-        </td>
-        <td className="edit-table-cell">
-            <PageLink path={`${ routes.EDIT_FORM }/${ car.id }`}>
-                <span className="edit-table-cell__icon edit-table-cell__icon-pen">
-                        &#9998;
                 </span>
-            </PageLink>
-        </td>
-    </tr>
-);
+            </td>
+            <td className="edit-table-cell">
+                <PageLink path={`${routes.EDIT_FORM}/${id}`}>
+                    <span className="edit-table-cell__icon edit-table-cell__icon-pen">
+                        &#9998;
+                    </span>
+                </PageLink>
+            </td>
+        </tr>
+    )
+};
 
 export default CarsRow;
