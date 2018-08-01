8839f0fbdda28ffc447d047ca7f09dfcbd99bc09
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-01 12:04:26 +0300

    Refactored car reducer.

--- src/app/reducers/carsReducer.js
+++ src/app/reducers/carsReducer.js
@@ -2,7 +2,6 @@ import actionTypes from '../constants/actionTypes';
 
 const carReducer = (state = [], action) => {
     const newCar = action.car;
-    const deleteId = action.id;
 
     switch (action.type) {
     case actionTypes.ADD_CAR:
@@ -10,25 +9,22 @@ const carReducer = (state = [], action) => {
             ...state,
             newCar
         ];
-    case actionTypes.EDIT_CAR: {
-        const carIndex = state.findIndex(car => car.id === newCar.id);
-        if (carIndex >= 0) {
-            return state.reduce((newState, currentCar) => {
-                if(currentCar.id === newCar.id) {
-                    const newCurrentCar = { ...currentCar, ...newCar };
-                    newState.push(newCurrentCar);
-                    return newState;
-                }
-                else {
-                    newState.push(currentCar);
-                    return newState;
-                }
-            }, []);
-        }
+    case actionTypes.EDIT_CAR:
+        return state.reduce((newState, currentCar) => {
+            if(currentCar.id === newCar.id) {
+                const newCurrentCar = { ...currentCar, ...newCar };
+                newState.push(newCurrentCar);
+                return newState;
+            }
+            else {
+                newState.push(currentCar);
+                return newState;
+            }
+        }, []);
+    case actionTypes.DELETE_CAR: {
+        const deleteCarId = action.id;
+        return state.filter(car => car.id !== deleteCarId);
     }
-        break;
-    case actionTypes.DELETE_CAR:
-        return state.filter(car => car.id !== deleteId);
     default:
         return state;
     }
