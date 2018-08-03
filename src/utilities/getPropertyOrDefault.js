864ca9a144e115800dff85f8b89603f6bac4a39f
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-03 15:58:39 +0300

    Created url utility.

--- src/utilities/getPropertyOrDefault.js
+++ src/utilities/getPropertyOrDefault.js
@@ -1,3 +1,1 @@
-const getPropertyOrDefault = (object, getter, defaultValue = '') => getter(object) ? getter(object) : defaultValue;
-
-export default getPropertyOrDefault;
+export const getPropertyOrDefault = (object, getter, defaultValue = '') => getter(object) ? getter(object) : defaultValue;
