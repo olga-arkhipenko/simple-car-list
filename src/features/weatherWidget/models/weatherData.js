53a231477b3c418eeb7a9fd2b7ab839c20ecdcfb
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-07 16:26:02 +0300

    Created weather data model.

---
+++ src/features/weatherWidget/models/weatherData.js
@@ -0,0 +1,23 @@
+class WeatherData {
+    city = null;
+
+    temperature = null;
+
+    pressure = null;
+
+    windDirection = null;
+
+    humidityPersentage = null;
+
+    cloudCoveragePersentage = null;
+
+    weatherDescription = null;
+
+    dayPart = null;
+
+    sunsetTime = null;
+
+    iconCode = null;
+}
+
+export default WeatherData;
