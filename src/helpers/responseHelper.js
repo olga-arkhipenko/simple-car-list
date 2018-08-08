1db557e953fc7c02dd6d3e72bbfd5cd84023805e
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-08 09:55:34 +0300

    Added ajax helpers.

---
+++ src/helpers/responseHelper.js
@@ -0,0 +1,8 @@
+export default {
+    getResponse(promise) {
+        return promise
+            .then(response => (response.ok
+                ? response.json()
+                : (function () { throw new Error(response.statusText); }())));
+    }
+};
